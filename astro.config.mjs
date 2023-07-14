284208ce361657f4d68f13ce586413aa8309ed2d
Dylan Awalt-Conley <web@dylan.ac>
2023-07-14 19:12:31 -0400

    add client:tina directive
    only hydrate in visual editor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import react from "@astrojs/react";
+
+const tina = ({
+  directiveName = 'tina'
+} = {}) => ({
+  name: 'tina-cms',
+  hooks: {
+    'astro:config:setup': ({ addClientDirective, opts }) => {
+      addClientDirective({
+        name: directiveName,
+        entrypoint: './client-directives/tina.mjs',
+      })
+    }
+  }
+})
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), tina()]
 });
---
+++ client-directives/tina.mjs
@@ -0,0 +1,9 @@
+/**
+ * @type {import('astro').ClientDirective}
+ */
+export default (load, opts, el) => {
+  const isEditor = window.top.location.pathname.startsWith('/admin')
+  if (isEditor) {
+    load().then(hydrate => hydrate())
+  }
+}
